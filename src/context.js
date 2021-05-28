c2ab5f6323cde1db17ca6a1e0f64e51b0ee4b46f
jkbsck <sucik.jakub@gmail.com>
2021-05-28 15:00:20 +0200

    Shows fetching basics implemented.

--- src/context.js
+++ src/context.js
@@ -1,9 +1,54 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect, useCallback } from "react";
+
+const url = "http://api.tvmaze.com/search/shows?q=";
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value="noah">{children}</AppContext.Provider>;
+  const [shows, setShows] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("girls");
+
+  const fetchShows = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${url}${searchTerm}`);
+      const data = await response.json();
+      // console.log(data);
+
+      if (data.length > 0) {
+        const newShows = data.map((show) => {
+          return {
+            id: show.show.id,
+            name: show.show.name,
+            genres: show.show.genres,
+            premiered: show.show.premiered,
+            rating: show.show.rating,
+            image: show.show.image,
+            summary: show.show.summary,
+          };
+        });
+        setShows(newShows);
+      } else {
+        setShows([]);
+      }
+      // const newShows = data
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+    console.log(shows);
+  }, [searchTerm]);
+
+  useEffect(() => {
+    fetchShows();
+  }, [searchTerm, fetchShows]);
+
+  return (
+    <AppContext.Provider value={{ setSearchTerm, shows, loading }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 const useGlobalContext = () => {
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useRef } from "react";
+import { BiSearch } from "react-icons/bi";
+import { useGlobalContext } from "../context";
 
 const Shop = () => {
-  return <div>shop</div>;
+  const { setSearchTerm, shows, loading } = useGlobalContext();
+  const searchValue = useRef("");
+
+  const handleSearch = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+  console.log(shows);
+  return (
+    <section className="shop">
+      <div className="form-wrapper">
+        <form className="search-form">
+          <input
+            className="search-input"
+            type="text"
+            id="search"
+            name="search"
+            ref={searchValue}
+            onChange={handleSearch}
+          />
+          <button className="search-btn btn" type="submit">
+            <BiSearch />
+          </button>
+        </form>
+      </div>
+      <div className="shows-wrapper">
+        {shows.map((show) => {
+          return (
+            <div key={show.id}>
+              <span>name:{show.name}</span>
+              <span>genre:{show.genres[0]}</span>
+              <img src={show.image.original} alt={show.name} />
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Shop;
+
+// id: show.show.id,
+//             name: show.show.name,
+//             genres: show.show.genres,
+//             premiered: show.show.premiered,
+//             rating: show.show.rating,
+//             image: show.show.image.original,
+//             summary: show.show.summary,
